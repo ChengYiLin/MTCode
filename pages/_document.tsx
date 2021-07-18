fde91f37b3679ca1115b909176949bf0deaea50e
林政儀 <cyl@linzhengyideMac-mini.local>
2021-07-18 16:31:00 +0800

    chore: 加入 tailwindcss 及 Google Font
    Nextjs 多頁共用的 Document 元素可以放在 ./pages/._document.tsx 中
    
    相關參考連結
    * tailwindcss : https://tailwindcss.com/docs/guides/nextjs
    * Import Google Font in Nextjs : https://dev.to/thomasledoux1/the-best-way-to-use-google-fonts-in-next-js-tailwind-43a2
    * Nextjs Official Doc:
    	* https://nextjs.org/docs/basic-features/font-optimization
    	* https://nextjs.org/docs/messages/google-font-display

---
+++ pages/_document.tsx
@@ -0,0 +1,56 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="author" content="Cheng Yi Lin, Martin" />
+          <meta name="description" content="學習 NextJS" />
+          <link rel="icon" href="/favicon.ico" />
+          <meta property="og:title" content="Martin Coede" />
+          <meta property="og:image" content="/images/test.jpeg" />
+          <meta property="og:description" content="學習 NextJS" />
+          {/* Reset Css */}
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+            integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+            crossOrigin="anonymous"
+            referrerPolicy="no-referrer"
+          />
+          {/* Google Font */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
