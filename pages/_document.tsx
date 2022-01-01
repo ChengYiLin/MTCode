b67590242075f44ec0c3f1bf8238db67dff25a92
Cheng Yi Lin <cheng.yi.lin.217@gmail.com>
2022-01-01 23:57:13 +0800

    feat : add google fonts in the project

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,24 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
     render() {
         return (
-            <Html className="scroll-smooth">
-                <Head />
-                <body>
+            <Html className="scroll-smooth font-poppins">
+                <Head>
+                    {/* Google Fonts */}
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.gstatic.com"
+                        crossOrigin="anonymous"
+                    ></link>
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
+                <body className="bg-cyan-300">
                     <Main />
                     <NextScript />
                 </body>
