6c6972c16f700ce89ac7aa1954e40c03688a5f4e
Cheng Yi Lin <cheng.yi.lin.217@gmail.com>
2022-01-03 23:41:45 +0800

    style : remove unused import

--- layout/HeaderLayout/index.tsx
+++ layout/HeaderLayout/index.tsx
@@ -1,5 +1,5 @@
 import type { FC } from 'react';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from '../../components/header';
 
 const HeaderLayout: FC = ({ children }) => {
