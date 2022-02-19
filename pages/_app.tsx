6f60374c71349bdd39661c7190b2643ec67af2c1
Cheng Yi Lin <cheng.yi.lin.217@gmail.com>
2022-02-20 01:47:05 +0800

    feat: customize scroll bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import '../styles/scrollbar.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
