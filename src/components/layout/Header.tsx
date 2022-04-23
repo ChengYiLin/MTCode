64f22629228d93940131c19c613dda34a1ecc6a3
Cheng Yi Lin <cheng.yi.lin.217@gmail.com>
2022-04-23 16:23:43 +0800

    feat: Build the basic Dark Mode implementaion

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { FiSun, FiMoon } from "react-icons/fi";
 
 const navItemsList = [
     {
@@ -13,10 +14,24 @@ const navItemsList = [
 ];
 
 const Header: FC = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        const htmlTag = document.getElementsByTagName("html")[0];
+
+        isDarkMode
+            ? htmlTag.classList.add("dark")
+            : htmlTag.classList.remove("dark");
+    }, [isDarkMode]);
+
     return (
-        <header className="h-14 bg-indigo-200">
+        <header className="h-14">
             <div className="container mx-auto h-full flex">
-                <h1>This is Logo</h1>
+                <div className="flex items-center">
+                    <h1 className="font-extrabold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-emerald-400 to-cyan-500">
+                        Martin Code
+                    </h1>
+                </div>
                 <div className="ml-auto flex items-center">
                     <nav>
                         <ul className="flex">
@@ -30,6 +45,18 @@ const Header: FC = () => {
                             ))}
                         </ul>
                     </nav>
+                </div>
+                <div className="flex items-center">
+                    <div
+                        className="p-2.5 border rounded-full cursor-pointer dark:bg-slate-600"
+                        onClick={() => setIsDarkMode((prev) => !prev)}
+                    >
+                        {isDarkMode ? (
+                            <FiMoon className="text-xl dark:text-white" />
+                        ) : (
+                            <FiSun className="text-xl" />
+                        )}
+                    </div>
                 </div>
             </div>
         </header>
