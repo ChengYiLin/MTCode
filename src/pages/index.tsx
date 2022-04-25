13029b86eb45ea4318c9f48c2509fa6d43cdf636
Cheng Yi Lin <cheng.yi.lin.217@gmail.com>
2022-04-25 23:58:52 +0800

    feat: Build the hero image  background color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,17 @@ import Layout from "@components/layout/Layout";
 const Home: NextPage = () => {
     return (
         <Layout>
-            <h1 className="text-cyan-500">This is Content</h1>
+            {/* Who I am */}
+            <div className="p-4 h-75vh max-h-[800px] bg-gradient-to-b from-white to-emerald-300 dark:from-gray-800 dark:to-cyan-800">
+                <div className="container mx-auto h-full flex items-center">
+                    <div>
+                        <div className="dark:text-white">
+                            <p>Hi !</p>
+                            <h1>You can call me Martin</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Layout>
     );
 };
