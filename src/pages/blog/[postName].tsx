b39a9f7ae1925e091ba043ef43e8f536a9a4aedf
ChengYiLin <cheng.yi.lin.217@gmail.com>
2022-09-13 23:58:05 +0800

    feat: transfer from Markdown to the html in `Blog/post` page

--- src/pages/blog/[postName].tsx
+++ src/pages/blog/[postName].tsx
@@ -1,11 +1,66 @@
-import type { NextPage } from "next";
-import { useRouter } from "next/router";
+import fs from "fs";
+import { join } from "path";
+import type { NextPage, GetStaticProps, GetStaticPaths } from "next";
+import matter from "gray-matter";
+import md from "markdown-it";
 
-const Post: NextPage = () => {
-    const router = useRouter();
-    const { postName } = router.query;
+interface IPostPageProps {
+    title: string;
+    date: string;
+    tags: string[];
+    bannerImage: string;
+    author: string;
+    content: string;
+}
 
-    return <h1 className="bg-yellow-200 p-32 text-5xl">Blog - {postName}</h1>;
+const Post: NextPage<IPostPageProps> = (props) => {
+    const { title, date, tags, bannerImage, author, content } = props;
+    console.log(props);
+
+    return (
+        <div className="container mx-auto py-12">
+            <h1 className="text-2xl">{title}</h1>
+            <p>author : {author}</p>
+            <p>date : {date}</p>
+            <p>tags: {tags}</p>
+            <p>banner Image : {bannerImage}</p>
+            <div>
+                <p className="text-xl">Content</p>
+                <div
+                    dangerouslySetInnerHTML={{ __html: md().render(content) }}
+                />
+            </div>
+        </div>
+    );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const postDir = join(process.cwd(), "src/content/blog");
+    const postFiles = fs.readdirSync(postDir).map((fileName) => ({
+        params: {
+            postName: fileName.replace(".mdx", ""),
+        },
+    }));
+
+    return {
+        paths: postFiles,
+        fallback: false,
+    };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const postName = params?.postName as string;
+    const postPath = join(process.cwd(), "src/content/blog", `${postName}.mdx`);
+    const postFile = fs.readFileSync(postPath, "utf-8");
+
+    const { content, data } = matter(postFile);
+
+    return {
+        props: {
+            content,
+            ...data,
+        },
+    };
 };
 
 export default Post;
