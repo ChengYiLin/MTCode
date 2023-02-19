8f70e6d265c863daca1c95a80b33c7055e9a0036
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-02-19 23:59:57 +0800

    style: Adjust Logo hover behavior

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 const Logo = () => {
   return (
-    <div className="flex items-center gap-2 select-none cursor-pointer">
+    <div className="flex items-center gap-2 select-none cursor-pointer hover:text-rose-600">
       <Image src="/logo/rose.png" alt="Logo" width={48} height={48} />
-      <span className="text-[24px] hover:text-rose-600 transition-colors duration-500">
+      <span className="text-[24px] transition-colors duration-500">
         Martin Code
       </span>
     </div>
