db37c77978c0d3be6f1d93e8d3db6e77c5d1297b
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-09-10 14:44:05 +0800

    chore: Install EsLint & Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:astro/recommended',
+        'prettier',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+        {
+            files: ['*.astro'],
+            parser: 'astro-eslint-parser',
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+                extraFileExtensions: ['.astro'],
+            },
+            rules: {
+                'astro/no-set-html-directive': 'error',
+            },
+        },
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {},
+}
