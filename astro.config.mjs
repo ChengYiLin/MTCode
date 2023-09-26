4133b73e33b1cb2db5cf8a2a02fe2b49ae6ae0a0
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-09-27 03:05:49 +0800

    chore: Add @shadcn/ui to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
-
 import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    // Ref: https://ui.shadcn.com/docs/installation/astro#update-astro-tailwind-config
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 })
