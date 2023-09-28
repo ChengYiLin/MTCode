a7b8a5502ac4bed0fbcd2e9f028f3baa0b93a9e8
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-09-28 23:42:39 +0800

    Revert "chore: Add @shadcn/ui to project"
    This reverts commit 4133b73e33b1cb2db5cf8a2a02fe2b49ae6ae0a0.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    // Ref: https://ui.shadcn.com/docs/installation/astro#update-astro-tailwind-config
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [react(), tailwind()],
 })
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
