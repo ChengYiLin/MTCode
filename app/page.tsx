0e5d8baf1e8c19650edf738583a3fdcdcdf2002b
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-12-16 17:35:21 +0800

    feat: Remove unused layout

--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,10 @@ export default async function Page() {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
+            Hi, 我是 Martin
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
+            一名來自台灣的前端工程師, 非本科轉職者之一, 經營此部落格紀錄自己的所學所想
           </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
